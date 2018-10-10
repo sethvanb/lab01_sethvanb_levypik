63776149057560e2239b6d8346a7c2bffd51c7e2
Seth Vanbrocklin <vanbrocklin@cstl-19.cs.ucsb.edu>
2018-10-10 13:40:27 -0700

    Finished block.cpp

--- block.cpp
+++ block.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	int rows=1;
+	int columns=1;
+	while(rows>0&&columns>0){
+	cout<<"Enter number of rows and columns: "<<endl;
+	cin>>rows>>columns;
+	for(int i=0; i<rows;i++){
+		for(int j=0;j<columns;j++){
+			cout<<"X";
+		}
+		cout<<endl;
+	}
+	}
+	return 0;
+}
